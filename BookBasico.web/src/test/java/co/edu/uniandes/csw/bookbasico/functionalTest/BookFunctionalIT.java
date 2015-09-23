76a97b481b211cea157ea18d3cd31a4428ce1956
Jhonatan Alarcón <jj.alarcon10@uniandes.edu.co>
2015-09-23 15:51:04 -0500

    Se realiza las pruebas funcionales  para editar y eliminar un book Se realiza las pruebas funcionales  para editar y eliminar un book

--- BookBasico.web/src/test/java/co/edu/uniandes/csw/bookbasico/functionalTest/BookFunctionalIT.java
+++ BookBasico.web/src/test/java/co/edu/uniandes/csw/bookbasico/functionalTest/BookFunctionalIT.java
@@ -5,7 +5,6 @@
  */
 package co.edu.uniandes.csw.bookbasico.functionalTest;
 
-
 import co.edu.uniandes.csw.bookbasico.services.BookService;
 import java.io.File;
 import java.net.URL;
@@ -42,14 +41,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 @RunWith(Arquillian.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BookFunctionalIT {
-    
-     public static String URLRESOURCES = "src/main/webapp";
-     public static WebDriver driver;
-     // Está variable contendrá la URL que utiliza arquillian para ejecutar el contenedor
-    @ArquillianResource URL deploymentURL;
-    
+
+    public static String URLRESOURCES = "src/main/webapp";
+    public static WebDriver driver;
+    // Mediante la anotación @ArquillianResource se obtiene la URL de despliegue de la aplicación
+    @ArquillianResource
+    URL deploymentURL;
+
+    /**
+     * Metodo que crea el empaquetamiento y el despliegue de la aplicación
+     * BookBasico
+     *
+     * @return Archive - war
+     */
     @Deployment
-    public static Archive<?> createDeployment(){
+    public static Archive<?> createDeployment() {
         MavenDependencyResolver resolver = DependencyResolvers.use(MavenDependencyResolver.class).loadMetadataFromPom("pom.xml");
 
         WebArchive war = ShrinkWrap
@@ -62,7 +68,7 @@ public class BookFunctionalIT {
                 .addPackage(BookService.class.getPackage())
                 // Se agrega contenido estatico: html y módulos de javascript. 
                 .addAsWebResource(new File(URLRESOURCES, "index.html"))
-                .merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class).importDirectory(URLRESOURCES + "/src/").as(GenericArchive.class), "/src/", Filters.includeAll())                               
+                .merge(ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class).importDirectory(URLRESOURCES + "/src/").as(GenericArchive.class), "/src/", Filters.includeAll())
                 // El archivo que contiene la configuracion a la base de datos. 
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
                 // El archivo beans.xml es necesario para injección de dependencias. 
@@ -72,27 +78,31 @@ public class BookFunctionalIT {
 
         return war;
     }
-    
-    
+
     @BeforeClass
     public static void setUp() {
-        // Se crea una instancia del driver de firefox sobre el que se ejecutara la aplicación.
+        // Crea una instancia del driver de firefox sobre el que se ejecutara la aplicación.
         driver = new FirefoxDriver();
     }
-    
-    
+
     @Before
     public void setUpTest() {
         // El browser  a esta url. Se ejecuta al inicar cada uno de los métodos de prueba indicados con @Test
-        driver.get(deploymentURL + "index.html");
+        driver.get(deploymentURL.toString());
     }
-    
-     @AfterClass
+
+    @AfterClass
     public static void tearDown() throws Exception {
         //Se ejecuta al terminar todos los métodos de prueba indicados con @Test Cierra el browser
         driver.quit();
     }
-    
+
+    /**
+     * El test crea un libro y luego verifica que el libro creado haya sido
+     * publicado correctamente en la galeria.
+     *
+     * @throws java.lang.InterruptedException
+     */
     @Test
     @RunAsClient
     public void t1createBook() throws InterruptedException {
@@ -108,15 +118,15 @@ public class BookFunctionalIT {
         driver.findElement(By.id("isbn")).clear();
         driver.findElement(By.id("isbn")).sendKeys("1025789845-13");
         driver.findElement(By.id("image")).clear();
-        driver.findElement(By.id("image")).sendKeys("http://image.casadellibro.com/a/l/t0/08/9788497592208.jpg");          
+        driver.findElement(By.id("image")).sendKeys("http://image.casadellibro.com/a/l/t0/08/9788497592208.jpg");
         driver.findElement(By.id("save-book")).click();
         Thread.sleep(2000);
         List<WebElement> books = driver.findElements(By.xpath("//div[contains(@ng-repeat,'record in records')]"));
         for (WebElement book : books) {
             WebElement image = book.findElement(By.xpath("//img[contains(@ng-src,'http://image.casadellibro.com/a/l/t0/08/9788497592208.jpg')]"));
             List<WebElement> captions = book.findElements(By.xpath("//div[contains(@class, 'caption')]/p"));
-            if (captions.get(0).getText().contains("Cien años de Soledad") && captions.get(1).getText().contains("Realismo mágico") && 
-                captions.get(2).getText().contains("1025789845-13") && image.isDisplayed()){    
+            if (captions.get(0).getText().contains("Cien años de Soledad") && captions.get(1).getText().contains("Realismo mágico")
+                    && captions.get(2).getText().contains("1025789845-13") && image.isDisplayed()) {
                 success = true;
             }
         }
@@ -124,9 +134,56 @@ public class BookFunctionalIT {
         Thread.sleep(1000);
     }
 
-    
-    
-    
-    
-    
+    /**
+     * El test edita un libro ya creado y luego verifica que el libro editado
+     * haya sido publicado correctamente en la galeria.
+     *
+     * @throws java.lang.InterruptedException
+     */
+    @Test
+    @RunAsClient
+    public void t2EditBook() throws InterruptedException {
+        Thread.sleep(1500);
+        boolean success = false;
+        String newDescription = "Considerada una obra maestra de la literatura hispanoamericana y universal";
+        String newIsbn = "5555500000-13";
+        driver.findElement(By.id("-edit-btn")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("description")).clear();
+        driver.findElement(By.id("description")).sendKeys(newDescription);
+        driver.findElement(By.id("isbn")).clear();
+        driver.findElement(By.id("isbn")).sendKeys(newIsbn);
+        driver.findElement(By.id("save-book")).click();
+        Thread.sleep(1000);
+        List<WebElement> books = driver.findElements(By.xpath("//div[contains(@ng-repeat,'record in records')]"));
+        for (WebElement book : books) {
+            List<WebElement> captions = book.findElements(By.xpath("//div[contains(@class, 'caption')]/p"));
+            if (captions.get(1).getText().contains(newDescription)
+                    && captions.get(2).getText().contains(newIsbn)) {
+                success = true;
+            }
+        }
+        assertTrue(success);
+    }
+
+    /**
+     * El test elimina un libro de la galeria. Luego, verifica que el libro
+     * eliminado haya sido removido correctamente de la misma.
+     *
+     * @throws java.lang.InterruptedException
+     */
+    @Test
+    @RunAsClient
+    public void t3DeleteBook() throws InterruptedException {
+        Thread.sleep(1500);
+        boolean success = false;
+        driver.findElement(By.id("-delete-btn")).click();
+        Thread.sleep(1000);
+        List<WebElement> books = driver.findElements(By.xpath("//div[contains(@ng-repeat,'record in records')]"));
+        if (books.isEmpty()) {
+            success = true;
+        }
+        assertTrue(success);
+    }
+
 }
