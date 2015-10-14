f2eee942042563b2ca5c56221647c29bc295ea3e
Andrés Felipe Esguerra Restrepo <kaosterra@gmail.com>
2015-10-14 18:20:13 -0500

    uso libreria authService

---
+++ BookBasico.web/src/main/java/co/edu/uniandes/csw/bookbasico/services/RestConfig.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.csw.bookbasico.services;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("webresources")
+public class RestConfig extends ResourceConfig {
+
+    public RestConfig() {
+        packages("co.edu.uniandes.csw.bookbasico.services");
+        packages("co.edu.uniandes.csw.bookbasico.providers");
+        packages("co.edu.uniandes.csw.auth.service");
+        packages("co.edu.uniandes.csw.auth.filter");
+    }
+}
--- BookBasico.web/src/main/webapp/src/app.js
+++ BookBasico.web/src/main/webapp/src/app.js
@@ -2,16 +2,30 @@
 
     var mainApp = ng.module('mainApp', [
         'bookModule',
+        'authModule',
         'ngRoute'
     ]);
 
     mainApp.config(['$routeProvider', function ($routeProvider) {
             $routeProvider
-                .when('/book', {
-                    templateUrl: 'src/modules/book/book.tpl.html',
-                    controller: 'bookCtrl',
-                    controllerAs: 'ctrl'
-                })
-                .otherwise('/book');
+                    .when('/book', {
+                        templateUrl: 'src/modules/book/book.tpl.html',
+                        controller: 'bookCtrl',
+                        controllerAs: 'ctrl'
+                    })
+                    .otherwise('/book');
+        }]);
+    mainApp.config(['authServiceProvider', function (auth) {
+            auth.setValues({
+                apiUrl: 'users',
+                successPath: '/catalog',
+                loginPath: '/login',
+                registerPath: '/register',
+                logoutRedirect: '/login',
+                loginURL: 'login',
+                registerURL: 'register',
+                logoutURL: 'logout',
+                nameCookie: 'userCookie'
+            });
         }]);
 })(window.angular);
