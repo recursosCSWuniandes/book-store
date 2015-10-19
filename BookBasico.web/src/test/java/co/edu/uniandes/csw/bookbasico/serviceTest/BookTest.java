5c27fab55d4d3f87aa6683d567c725de54d21a3e
Jhonatan Alarcon <jhonatan.alarcon.ariza@gmail.com>
2015-10-18 22:42:10 -0500

    Shiro.ini de pruebas

--- BookBasico.web/src/test/java/co/edu/uniandes/csw/bookbasico/serviceTest/BookTest.java
+++ BookBasico.web/src/test/java/co/edu/uniandes/csw/bookbasico/serviceTest/BookTest.java
@@ -69,7 +69,7 @@ public class BookTest {
                 // El archivo beans.xml es necesario para injeccion de dependencias. 
                 .addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"))
                 // El archivo shiro.ini es necesario para injeccion de dependencias
-                .addAsWebInfResource(new File("src/main/webapp/WEB-INF/shiro.ini"))
+                .addAsWebInfResource("WEB-INF/shiro.ini")
                 // El archivo web.xml es necesario para el despliegue de los servlets
                 .setWebXML(new File("src/main/webapp/WEB-INF/web.xml"));
 
